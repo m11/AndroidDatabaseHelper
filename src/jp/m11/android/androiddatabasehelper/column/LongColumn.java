183caf251d5059f0c13d6897564e47abf890bdba
m11 <d@ddd.cx>
2012-07-19 00:59:02 +0900

    JSONへの出力とJSONからの入力機能の追加。

--- src/jp/m11/android/androiddatabasehelper/column/LongColumn.java
+++ src/jp/m11/android/androiddatabasehelper/column/LongColumn.java
@@ -1,8 +1,17 @@
 package jp.m11.android.androiddatabasehelper.column;
 
+import jp.m11.android.androiddatabasehelper.DatabaseException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class LongColumn extends Column<Long> {
 	public final static String DATA_TYPE_STRING = "INTEGER";
+
+	protected LongColumn() {
+		super();
+	}
 
 	public LongColumn( String columnName ) {
 		super( columnName );
@@ -20,5 +29,13 @@ public class LongColumn extends Column<Long> {
 	@Override
 	public String getDataTypeString() {
 		return DATA_TYPE_STRING;
+	}
+
+	@Override
+	public void setValue( JSONObject value, boolean overWriteAll ) throws JSONException {
+		if ( this._isImmutable ) {
+			throw new DatabaseException();
+		}
+		this._value = value.getLong( this.getColumnName() );
 	}
 }
